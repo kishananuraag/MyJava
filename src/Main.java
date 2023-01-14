fa10ebdacc1acfa97afc1177215a95d4848cda93
kishan <kishananuraag@gmail.com>
2023-01-14 12:00:36 +0530

    New commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] x = {{10,20,30},{40,50}};
+        int y = 10;
+        int z = 1;
+        for(int[] x1:x)
+        {
+            for(int x2:x1)
+            {
+                System.out.println(x2);
+            }
+        }
+        int a = 1;
+
     }
 }
+
